31addd30df0c513fee35fe4b4f9eb5b85bd62d06
mumthasshajahan <mumthassmk@gmail.com>
2023-11-02 21:52:18 +0530

    if else with booleans

---
+++ script.js
@@ -0,0 +1,20 @@
+//---------------"if" and "else" statements with boolean values in JavaScript-------------------------// 
+
+//----Boolean Values:---------//
+/*In JavaScript, there are two boolean values: true and false.
+ These values represent whether something is true or false.*/
+
+/*if (condition) {
+ Code to run if the condition is true}
+ else {
+Code to run if the condition is false}*/
+//..........................................
+
+/*let number=5;
+if (number %2==0){
+    console.log('The number is even')
+}
+else{
+    console.log('The number is odd')
+}
+*/
