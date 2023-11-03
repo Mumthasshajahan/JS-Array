a20026e956ca3e52649f73350362a36e7565673d
mumthasshajahan <mumthassmk@gmail.com>
2023-11-03 14:41:45 +0530

    if else with booleans

--- script.js
+++ script.js
@@ -8,8 +8,9 @@
  Code to run if the condition is true}
  else {
 Code to run if the condition is false}*/
-//..........................................
 
+// eg 1.
+//Check if a number is odd or even
 /*let number=5;
 if (number %2==0){
     console.log('The number is even')
@@ -18,3 +19,54 @@ else{
     console.log('The number is odd')
 }
 */
+//------------
+//eg 2.
+//Check if a number is positive or negative
+ /*let number = -5
+if(number>0){
+    console.log("Number is positive")
+}
+else if (number <0){
+    console.log("Number is Negative")
+}
+else{
+    console.log("Zero")
+}  */
+//------------------
+
+//eg 3.
+// Check if user is an adult or not
+/*let age=15;
+if(age>=18){
+    console.log("User is an Adult");
+}
+else{
+    console.log("User is Not an Adult");
+
+}*/
+//--------------------
+// eg 4.
+// Determine the largest of two numbers
+
+/*let num1 = 7; 
+let num2 = 5;
+
+if (num1 > num2) {
+    console.log("The first number is larger.");
+} else if (num1 < num2) {
+    console.log("The second number is larger.");
+} else {
+    console.log("Both numbers are equal.");
+}*/
+//----------------------------
+
+//eg 5.
+//Check if a user is old enough to vote
+/*let age=16;
+if (age>=18){
+    console.log('You are eligible to vote')
+}
+else{
+    console.log('you are not eligible to vote');
+}*/
+
